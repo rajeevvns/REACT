3715a10112eaad9514a92b92627f950d3eec230a
Banuprakash <banuprakashc@yahoo.co.in>
2024-04-19 11:13:28 +0530

    day 5

--- contactmanager/src/index.js
+++ contactmanager/src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom'
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
-
-ReactDOM.render(<Provider store={store}>
-  <App />
-</Provider>, document.getElementById('root') )
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
